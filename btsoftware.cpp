c00707b8ab299572e179cb50261c01b878718c70
Huỳnh Tấn Dĩnh <152977864+huynhtandinh2005@users.noreply.github.com>
2023-12-13 22:24:49 +0700

    Add files via upload

---
+++ btsoftware.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hello"<<endl;
+	cout<<"This is our exercise!\n";
+	cout<<"Comment your name to participate in the project:\n";
+	char per1[27]="1.Huynh Tan Dinh";
+	char per2[27];
+	char per3[27];
+	cout<<per1;
+}
