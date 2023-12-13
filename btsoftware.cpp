043dfad458fedfa1d394be7399daec218b3383ae
Nhthuando <151205383+Nhthuando@users.noreply.github.com>
2023-12-13 22:28:06 +0700

    Update btsoftware.cpp

--- btsoftware.cpp
+++ btsoftware.cpp
@@ -5,7 +5,7 @@ int main(){
 	cout<<"This is our exercise!\n";
 	cout<<"Comment your name to participate in the project:\n";
 	char per1[27]="1.Huynh Tan Dinh";
-	char per2[27];
+	char per2[27]="2.Ngo Huu Thuan";
 	char per3[27];
 	cout<<per1;
 }
